0fdc1f05029d8267e7cc6d16c6cb2e9eef3bf3f0
Sil_Palaoro <silvinapalaoro@gmail.com>
2026-09-15 10:52:53 -0300

    Separa chooseMove en archivo strategy.js

--- src/controllers/bot.controller.js
+++ src/controllers/bot.controller.js
@@ -1,36 +1,4 @@
-function chooseMove(state, jugador, semilla){
-
-    console.log(state);
-
-    let fichas = jugador.fichas;
-    let directions = ["N", "S", "E", "O"]
-
-    // Generador determinista mediante semilla
-
-    function crearGenerador(semilla) {
-      let s = semilla;
-      return function () {
-        s = (s * 1664525 + 1013904223) % 4294967296;
-        return s / 4294967296;
-      };
-    }
-
-    // Generador de movimiento
-
-    const generador = crearGenerador(semilla);
-    
-    const fichaIndex = Math.floor(generador() * fichas.length);
-    const directionIndex = Math.floor(generador() * directions.length);
-    
-    let fichaId = jugador.id + (fichaIndex + 1); 
-    let direction = directions[directionIndex];
-    let movement = {
-        "pieceId": fichaId,
-        "direction": direction
-    };
-
-    return movement;
-};
+import chooseMove from "../strategy.js";
 
 
 export class BotController {
